3ed76620f22fe55020deb245da6be25d5e175934
simran <simrans20@nycstudents.net>
2025-08-12 15:56:35 -0400

    edited some dialouge

--- Assets/TransitionScript.cs
+++ Assets/TransitionScript.cs
@@ -5,34 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class TransitionScript : MonoBehaviour
 {
-    public Animator transition;
-    public GameObject dialouge;
+    // animation
     public Animation image;
+    public Animator transition;
 
+    public GameObject dialouge; // finds dialouge
+    
     public float transitionTime;
     // Update is called once per frame
     void Update()
     {
-        if (dialouge.GetComponent<Dialouge>().sceneStop == true)
+        if (dialouge.GetComponent<Dialouge>().sceneStop == true) // finds when scene is stopped
         {
-            LoadNextLevel();
+            LoadNextLevel(); // loads next scene
         }
     }
 
     public void LoadNextLevel()
     {
-
-        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1)); // transitions to next scene according to index
     }
 
     IEnumerator LoadLevel(int levelIndex)
     {
-        transition.enabled = true;
-        image.Play();
-        transition.SetTrigger("Start");
+        transition.enabled = true; // starts transition at this time
+        image.Play(); // plays animation
+        transition.SetTrigger("Start"); // triggers animator
 
-        yield return new WaitForSeconds(transitionTime);
+        yield return new WaitForSeconds(transitionTime); // wait for few seconds
 
-        SceneManager.LoadScene(levelIndex);
+        SceneManager.LoadScene(levelIndex); // loads next scene
     }
 }
