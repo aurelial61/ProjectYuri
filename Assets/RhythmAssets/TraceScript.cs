dd46652e711edcfc9cf107d1a5f2d342449cc669
Aurelia Lei <jlei61@stuy.edu>
2025-08-12 13:02:42 -0400

    coded trace notes a bit and made minor adjustments

---
+++ Assets/RhythmAssets/TraceScript.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TraceScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    public GameObject manager;
+    
+    public float timer;
+    public float hitTime;
+    public GameObject perfect;
+    public GameObject miss;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer = manager.GetComponent<RhythmScript>().timer;
+        
+        transform.position -= new Vector3(speed, 0, 0) * Time.deltaTime;
+        
+        if ((Input.GetKey(KeyCode.Space)))
+        {
+            if ((hitTime - timer >= -0.02f && hitTime - timer <= 0.02f))
+            {
+                manager.GetComponent<RhythmScript>().score += 3;
+                manager.GetComponent<RhythmScript>().combo++;
+                Instantiate(perfect);
+                Destroy(gameObject);
+            }
+            
+            
+            
+        }
+        if (hitTime - timer < -0.02f)
+        {
+            manager.GetComponent<RhythmScript>().score -= 3;
+            manager.GetComponent<RhythmScript>().combo = 0;
+            Instantiate(miss);
+            manager.GetComponent<RhythmScript>().fullCombo = false;
+            manager.GetComponent<RhythmScript>().allPerfect = false;
+            Destroy(gameObject);
+        }
+
+    }
+}
