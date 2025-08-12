3b0e233557df9118c6c1448a12558878f7827403
simran <simrans20@nycstudents.net>
2025-08-12 14:31:10 -0400

    edited transition

--- Assets/TransitionScript.cs
+++ Assets/TransitionScript.cs
@@ -13,7 +13,7 @@ public class TransitionScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) && dialouge.GetComponent<Dialouge>().sceneStop == true)
+        if (dialouge.GetComponent<Dialouge>().sceneStop == true)
         {
             LoadNextLevel();
         }
