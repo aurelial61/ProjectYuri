a2e53a6d9270a4730167eeb6defb5e5399e16af6
Aurelia Lei <jlei61@stuy.edu>
2025-08-12 14:50:21 -0400

    completed hold notes

--- Assets/RhythmAssets/HoldScript.cs
+++ Assets/RhythmAssets/HoldScript.cs
@@ -9,29 +9,56 @@ public class HoldScript : MonoBehaviour
     public float duration;
     public GameObject manager;
     public GameObject notePrefab;
-    
+    public GameObject tracePrefab;
     public float timer;
     public float hitTime;
     public float endHitTime;
     public float spb;
     public float beatTimer;
+    public float spawnDistance;
     public GameObject perfect;
     public GameObject great;
     public GameObject good;
     public GameObject miss;
+    public GameObject releasePrefab;
     
+    public bool release;
     void Start()
     {
         endHitTime = hitTime + duration;
+        
+        GameObject thisNote = Instantiate(notePrefab, transform.position, Quaternion.identity);
+        thisNote.GetComponent<NoteScript>().speed = speed;
+        thisNote.GetComponent<NoteScript>().hitTime = hitTime;
+        thisNote.GetComponent<NoteScript>().manager = manager;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        GameObject thisNote = Instantiate(notePrefab, transform.position, Quaternion.identity);
-        thisNote.GetComponent<NoteScript>().speed = speed;
-        thisNote.GetComponent<NoteScript>().hitTime = hitTime;
-        thisNote.GetComponent<NoteScript>().manager = manager;
+        timer = manager.GetComponent<RhythmScript>().timer;
+        beatTimer += Time.deltaTime;
+        if (beatTimer > spb * 0.2f)
+        {
+            GameObject traceNote = Instantiate(tracePrefab, transform.position, Quaternion.identity);
+            traceNote.GetComponent<TraceScript>().speed = speed;
+            traceNote.GetComponent<TraceScript>().hitTime = timer + spawnDistance / speed;
+            traceNote.GetComponent<TraceScript>().manager = manager;
+            beatTimer = 0;
+            
+        }
+
+        if (timer > endHitTime - spawnDistance / speed)
+        {
+            GameObject thisNote = Instantiate(releasePrefab, transform.position, Quaternion.identity);
+            thisNote.GetComponent<ReleaseScript>().speed = speed;
+            thisNote.GetComponent<ReleaseScript>().hitTime = endHitTime;
+            thisNote.GetComponent<ReleaseScript>().manager = manager;
+            Destroy(gameObject);
+        }
+
+        
 
     }
 }
---
+++ Assets/RhythmAssets/ReleaseScript.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReleaseScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    public GameObject manager;
+    
+    public float timer;
+    public float hitTime;
+    public GameObject perfect;
+    public GameObject great;
+    public GameObject good;
+    public GameObject miss;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer = manager.GetComponent<RhythmScript>().timer;
+        
+        transform.position -= new Vector3(speed, 0, 0) * Time.deltaTime;
+        
+        if ((Input.GetKeyUp(KeyCode.Space)))
+        {
+            if ((hitTime - timer <= 0.15f && hitTime - timer > 0.1f) || (hitTime - timer < -0.1f && hitTime - timer >= -0.15f))
+            {
+                manager.GetComponent<RhythmScript>().score += 5;
+                manager.GetComponent<RhythmScript>().combo = 0;
+                manager.GetComponent<RhythmScript>().fullCombo = false;
+                manager.GetComponent<RhythmScript>().allPerfect = false;
+                Instantiate(good);
+                
+            }
+            else if ((hitTime - timer <= 0.10f && hitTime - timer > 0.02f) || (hitTime - timer < -0.02f && hitTime - timer >= -0.1f))
+            {
+                manager.GetComponent<RhythmScript>().score += 10;
+                manager.GetComponent<RhythmScript>().combo++;
+                manager.GetComponent<RhythmScript>().allPerfect = false;
+                Instantiate(great);
+            }
+            else if ((hitTime - timer >= -0.02f && hitTime - timer <= 0.02f))
+            {
+                manager.GetComponent<RhythmScript>().score += 15;
+                manager.GetComponent<RhythmScript>().combo++;
+                Instantiate(perfect);
+            }
+            else if (hitTime - timer < 0.25f)
+            {
+                manager.GetComponent<RhythmScript>().score -= 5;
+                manager.GetComponent<RhythmScript>().combo = 0;
+                manager.GetComponent<RhythmScript>().fullCombo = false;
+                manager.GetComponent<RhythmScript>().allPerfect = false;
+                Instantiate(miss);
+            }
+
+            if (hitTime - timer <= 0.25f)
+            {
+                Destroy(gameObject);
+            }
+            
+        }
+        if (hitTime - timer < -0.25f)
+        {
+            manager.GetComponent<RhythmScript>().score -= 5;
+            manager.GetComponent<RhythmScript>().combo = 0;
+            Instantiate(miss);
+            manager.GetComponent<RhythmScript>().fullCombo = false;
+            manager.GetComponent<RhythmScript>().allPerfect = false;
+            Destroy(gameObject);
+        }
+
+    }
+}
